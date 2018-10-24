2942a29fc93eb4518bb52f4f838930c9ede29466
Katie McCulloch <katiee_mcculloch@yahoo.com>
2018-10-23 23:07:19 -0700

    fixes workflow from signup to linksocialmedia

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,7 +3,7 @@ import SignUpForm from "../SignUp/index.jsx";
 import LoginForm from "../Login/index.jsx";
 import Navbar from "../Navbar/index";
 import Dashboard from "../Dashboard/index";
-import LinkSocialMedia from "../LinkSocialMedia/index"
+import LinkSocialMedia from "../LinkSocialMedia/index";
 
 class App extends Component {
   constructor(props) {
@@ -49,17 +49,25 @@ class App extends Component {
 
   render() {
     var view;
-    if (this.state.loggedIn === true && this.state.linkedSocialMedia === false) {
-      view = <LinkSocialMedia/>
+    if (
+      this.state.loggedIn === true &&
+      this.state.linkedSocialMedia === false
+    ) {
+      view = <LinkSocialMedia />;
     } else if (
-      this.state.alreadyHasCredentials === true &&
+      this.state.linkedSocialMedia === true &&
       this.state.loggedIn === true
     ) {
       view = <Dashboard />;
     } else if (this.state.alreadyHasCredentials === true) {
       view = <LoginForm userVerified={this.userVerified} />;
     } else {
-      view = <SignUpForm loginButton={this.loginButton} />;
+      view = (
+        <SignUpForm
+          loginButton={this.loginButton}
+          userVerified={this.userVerified}
+        />
+      );
     }
     return (
       <div>
