04b739b9efd97ef941df3f2c7a6abce651428069
Katie McCulloch <katiee_mcculloch@yahoo.com>
2018-10-23 21:12:39 -0700

    adds index file for LinkSocialMedia component

---
+++ src/components/LinkSocialMedia/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import appStyles from "../../styles/App.css";
+
+class LinkSocialMedia extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return <div>Link your social media!</div>;
+  }
+}
+
+export default LinkSocialMedia;
